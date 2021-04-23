0b24554b1239280963a49c23529c7cf8a05bc437
Dexess <75804467+Dexess@users.noreply.github.com>
2021-04-23 13:10:09 +0300

    Add files via upload

--- test1.c
+++ test1.c
@@ -2,15 +2,28 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
+#include<unistd.h>
+#include<signal.h>
 
 int *selectionSort(int[],int);
 int *insertionSort(int[],int);
 void createFile(int, int);
+void signalHandlerUSR1(int);
+void signalHandlerUSR2(int);
+void childOperations(int);
+void forks(int);
+
+int waitTillDay5=0;
+
+struct Data{
+    int number;
+    int *array;
+};
 
 int main(int argc, char const *argv[])
 {   
     srand(time(0));
-    int n,m;
+    int n,m,i;
     printf("Welcome to app\n");
     printf("Enter number of file: ");
     scanf("%d",&n);
@@ -18,8 +31,39 @@ int main(int argc, char const *argv[])
     scanf("%d",&m);
     
     createFile(m,n);
+
+    forks(n);
     
+    printf("Back\n");
     return 0;
+}
+
+void forks(int n){
+    
+    int i;
+    pid_t pid[n];
+    for(i = 0; i < n; i++){ 
+        if((pid[i] = fork()) == 0){
+            printf("I am here");
+            signal(SIGUSR2,signalHandlerUSR2);
+            signal(SIGUSR1,signalHandlerUSR1);
+            while(waitTillDay5)
+                sleep(1);
+            //childOperations(i);
+            
+               
+        }
+      
+    }
+    for(i = 0; i < n; i++){
+        if(pid[i] % 2 == 0){
+                      
+            kill(pid[i],SIGUSR2);
+        }
+        else{
+            kill(pid[i],SIGUSR1);
+        }
+    }
 }
 
 void createFile(int m, int n){
@@ -80,3 +124,49 @@ int *insertionSort(int *myList, int size){
     return myList;
 }
 
+void signalHandlerUSR1(int signumber){
+    waitTillDay5 = 1;
+}
+
+void signalHandlerUSR2(int signumber){
+   waitTillDay5 = 1;
+}
+
+void childOperations(int fileNum){
+    printf("hadi be brom3");
+    int i=0,j;
+    FILE *myFile;
+    char filename[20],filename2[20];
+    sprintf(filename,"input%d.txt",fileNum);
+    
+    myFile = fopen(filename,"r");
+
+    struct Data *myData = malloc(sizeof(struct Data));
+
+    if(myFile != NULL){
+        while(i < 2){
+            if(i == 0){
+                if(fscanf(myFile,"%d\n",&myData->number) != EOF);
+            }
+            else{
+                myData->array = (int*)malloc(myData->number*sizeof(int));
+                for(j = 0; j < myData->number; j++){
+                    if(fscanf(myFile,"%d ",&myData->array[j])!= EOF);
+                }
+                
+            }
+        }
+    }
+    fclose(myFile);
+
+    myData->array = selectionSort(myData->array,myData->number);
+    sprintf(filename2,"output%d.txt",fileNum);
+    
+    myFile = fopen(filename2,"w");
+    fprintf(myFile,"%d\n",myData->number);
+        for(j = 0; j < myData->number; j++){
+            fprintf(myFile,"%d ",myData->array[j]);
+        }
+    
+
+}
